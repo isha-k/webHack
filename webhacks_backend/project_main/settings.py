d855c875fa0a2fe76852df8dcfdf388ea112fd12
Arjun <arjun@Arjuns-MacBook-Pro.local>
2024-05-04 11:38:10 +1000

    updated requirements.txt

--- webhacks_backend/project_main/settings.py
+++ webhacks_backend/project_main/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a$l5&1ia-i%j9i3@_r^(=@&amod!_o)@)w!j+osctc&-lrg@)f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
