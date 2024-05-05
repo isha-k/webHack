99532efab6f5334a378711b55b28bb8c2d6f8144
Arjun <arjun@dyn-118-138-77-141.its.monash.edu.au>
2024-05-05 17:01:30 +1000

    updated the home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,17 +4,26 @@ import logo from './logo.png';
 export default function App() {
   return (
     <div>
-    <div style={{
-      position: 'fixed',
-      top: 0,
-      left: '50%',
-      maxWidth: '10%',
-      transform: 'translateX(-50%)',
-      zIndex: 1000 // Ensures the logo stays on top
-    }}>
-    <img src={logo} alt="Logo" /> 
+      <div style={{
+        position: 'fixed',
+        top: 0,
+        left: '50%',
+        maxWidth: '10%',
+        transform: 'translateX(-50%)',
+        zIndex: 1000 // Ensures the logo stays on top
+      }}>
+        <img src={logo} alt="Logo" /> 
+      </div>
+      <Spline 
+        scene="https://prod.spline.design/ZXXSJidIOfEfKttp/scene.splinecode" 
+        style={{
+          position: 'fixed',
+          width: '100%',
+          height: '100%',
+          top: 0,
+          left: 0
+        }}
+      />
     </div>
-    <Spline scene="https://prod.spline.design/ZXXSJidIOfEfKttp/scene.splinecode" />
-  </div>
   );
 }
